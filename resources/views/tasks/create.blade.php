46291098ca05f4f503da21ec586cfbc7a2031f59
The new begin <61123810@g.cmru.ac.th>
2021-11-18 11:30:07 +0700

    new todo

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Todo App Demo</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+
+        <h1>Task list</h1>
+        <from>
+            <div class="from-group">
+                <label for="descripion"> Task Description</label>
+                <input class="form-control" name="descripion"/>
+
+            </div>
+
+        </from>
+
+    </body>
+</html>
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Todo App Demo</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+
+        <h1>Task list</h1>
+
+    </body>
+</html>
